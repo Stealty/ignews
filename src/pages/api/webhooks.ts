de2735f43ea24b48f27779953016371b83698ee2
Exp <biellbigama@gmail.com>
2022-03-16 20:50:37 -0300

    Saving event data

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import { stripe } from "../../services/stripe";
 import Stripe from "stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -40,12 +41,28 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     }
 
     const { type } = event;
-
     if (relevantEvents.has(type)) {
-      console.log("evento recebido", event);
+      try {
+        switch (type) {
+          case "checkout.session.completed":
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            );
+
+            break;
+          default:
+            throw new Error("Unhandled event.");
+        }
+      } catch (err) {
+        return res.json({ error: "Webhook handler failed" });
+      }
     }
 
-    res.status(200).json({ received: true });
+    res.json({ received: true });
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method not allowed");
