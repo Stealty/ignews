6d3aa2ef69538fa569193bdf13b3fd286ec8395e
Exp <biellbigama@gmail.com>
2022-03-20 15:33:57 -0300

    feat/Page: Post(slug)

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,9 @@
-/* eslint-disable react/no-unescaped-entities */
 import Prismic from "@prismicio/client";
 import { GetStaticProps } from "next";
 import getPrismicClient from "../../services/prismic";
 import Head from "next/head";
 import styles from "./styles.module.scss";
+import Link from "next/link";
 
 type Post = {
   slug: string;
@@ -25,12 +25,14 @@ export default function Posts({ posts }: PostsProps) {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          { posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+          {posts.map((post) => (
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
@@ -49,12 +51,12 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   );
 
-  const posts = response.results.map(post => {
+  const posts = response.results.map((post) => {
     return {
       slug: post.uid,
       title: post.data.title,
       excerpt:
-        post.data.content.find(content => content.type === "paragraph")
+        post.data.content.find((content) => content.type === "paragraph")
           ?.text ?? "",
       updatedAt: new Date(post.last_publication_date).toLocaleDateString(
         "pt-BR",
